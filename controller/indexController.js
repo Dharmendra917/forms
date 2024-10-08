a9312b4ad025bb5a0633e0685e8443005dbf7bb1
Dharmendra <dp903604@gmail.com>
2024-10-08 15:10:56 +0530

    update

--- controller/indexController.js
+++ controller/indexController.js
@@ -2,6 +2,7 @@ const formModel = require("../models/indexModel");
 const imagekit = require("../utils/imageKit.js").initImageKit();
 const documentModel = require("../models/documentModel.js");
 const academicModel = require("../models/academicModel.js");
+const { generateNumericOTP } = require("../middlewares/uniqueIdGenterator.js");
 exports.home = async (req, res, next) => {
   try {
     res.status(200).json({ message: "This is Home Route" });
@@ -12,6 +13,7 @@ exports.home = async (req, res, next) => {
 
 exports.create = async (req, res, next) => {
   try {
+    const otp = generateNumericOTP(10);
     const { name, email, contact, dob } = req.body;
     const file = req.file;
     const modified = Date.now() + file.originalname;
@@ -23,9 +25,9 @@ exports.create = async (req, res, next) => {
       ...req.body,
       createdBy: req.id,
       image: { fileId: fileId, url },
+      userId: "U" + otp,
     };
     const responseData = await formModel(reqData).save();
-
     res.json({ message: "Form Submitted Successfully!" });
   } catch (error) {
     res.json(error);
@@ -88,6 +90,7 @@ exports.updateUser = async (req, res, next) => {
 exports.uploadDocument = async (req, res, next) => {
   const file = req.file;
   const { title } = req.body;
+  const otp = generateNumericOTP(10);
   const modified = Date.now() + file.originalname;
   const { fileId, url } = await imagekit.upload({
     file: file.buffer,
@@ -95,7 +98,12 @@ exports.uploadDocument = async (req, res, next) => {
   });
 
   const reqData = {
-    personalDocument: { fileId: fileId, url: url, title: title },
+    documentId: "D" + otp,
+    personalDocument: {
+      fileId: fileId,
+      url: url,
+      title: title,
+    },
   };
   const doc = await documentModel(reqData).save();
   const user = await formModel.findById(req.params.userId).exec();
@@ -109,7 +117,9 @@ exports.uploadDocument = async (req, res, next) => {
 
 exports.uploadAcademicDetails = async (req, res, next) => {
   const { body } = req;
-  const academic = await academicModel(body).save();
+  const otp = generateNumericOTP(10);
+  const reqData = { ...req.body, academicId: otp };
+  const academic = await academicModel(reqData).save();
   const user = await formModel.findById(req.params.userId).exec();
   if (!user) {
     return res.status(500).json({ message: "User Not Found" });
